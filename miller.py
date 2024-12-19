b6a9cfdaf336e846ba6ed3899ba1ebd5fffcec45
ruairimc01 <ruairi.mc.cabe01@gmail.com>
2024-12-19 16:00:10 +0000

    Fixed miller.py with ruff checks

--- miller.py
+++ miller.py
@@ -1,15 +1,15 @@
-from numpy import *
-
-A=2.2
-kappa=1.5
-delta=0.3
-R0=2.5
-theta=linspace(0,2*pi)
-r=R0/A
-R_s=R0+r*cos(theta+(arcsin(delta)*sin(theta)))
-Z_s=kappa*r*sin(theta)
-
+import numpy as np
 import matplotlib.pyplot as plt
+
+A = 2.2
+kappa = 1.5
+delta = 0.3
+R0 = 2.5
+theta = np.linspace(0, 2 * np.pi)
+r = R0 / A
+R_s = R0 + r * np.cos(theta + (np.arcsin(delta) * np.sin(theta)))
+Z_s = kappa * r * np.sin(theta)
+
 
 plt.plot(R_s, Z_s)
 plt.axis("equal")
